e171ef735be154e4f386490245d4187540d1b0b9
KyuSSeo <kyuri2665@gmail.com>
2025-04-29 15:44:57 +0900

    [Create] NpcAnimaitions
    NPC 움직임을 위한 기본 파일 작성

--- Assets/MSK/MSKScripts/NPCController.cs
+++ Assets/MSK/MSKScripts/NPCController.cs
@@ -1,14 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCController : MonoBehaviour, IInteractable
 {
     [SerializeField] Dialog dialog;
-    public void Interact()
+
+	private NPCAnimations NpcAnim;
+	private void Awake()
+	{
+		NpcAnim = GetComponent<NPCAnimations>();
+	}
+	public void Interact()
     {
-        Debug.Log("NPC Interact success");
 		if (DialogManager.Instance.isTyping == false) { 
         StartCoroutine(DialogManager.Instance.ShowText(dialog));
 		}
 	}
-
 }
