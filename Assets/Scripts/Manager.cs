82c31115bd030a163adfb4424a44a0a9c4add7bd
rnrgll <shuajjjjj@naver.com>
2025-04-29 14:11:44 +0900

    [Feat] DataManager 생성 및 Manager에 등록

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public static class Manager
 {
+	//사용하는 매니저 등록
 	public static UIManager UI { get { return UIManager.GetInstance(); } }
-
+	public static DataManager Data { get { return DataManager.GetInstance(); } }
+	//씬 매니저, 다이얼로그 매니저, 게임 매니저 등.. 을 생성해서 등록
+	
+	
 	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
 	private static void Initialize()
 	{
+		//각종 매니저를 생성
 		UIManager.CreateInstance();
+		DataManager.CreateInstance();
 	}
 }
