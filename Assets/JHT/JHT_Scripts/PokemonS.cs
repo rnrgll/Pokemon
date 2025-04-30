3b46e022e4791ed1df8f8e9aabaa33bc989e46f6
JangHyeonTae <141475959+JangHyeonTae@users.noreply.github.com>
2025-04-30 15:24:14 +0900

    [Feat] Add Dictionary Keys
    재훈님 스크립트 + 재훈님 깃허브 참조해서 딕셔너리에 Key, value생성

---
+++ Assets/JHT/JHT_Scripts/PokemonS.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonS : MonoBehaviour
+{
+	public int id;
+	public string pokeName;
+	public int level;
+	public int hp;
+	public int maxHp;
+	public int curExp;
+	public int nextExp;
+	[SerializeField] public PokemonStatS baseStat;   // 종족값
+	[SerializeField] public PokemonIVS iv;           // 개체값
+													//public PokemonStat ev;			// 노력치
+	[SerializeField] public PokemonStatS pokemonStat;    // 현재수치
+	public PokeType pokeType1;
+	public PokeType pokeType2;
+
+	public bool isDead;
+
+	public PokemonS(int _id, string _name, int _level, PokemonStatS _baseStat, PokemonIVS _iv, PokeType _pokeType1, PokeType _pokeType2)
+	{
+		id = _id;
+		pokeName = _name;
+		level = _level;
+		baseStat = _baseStat;
+		iv = _iv;
+		pokeType1 = _pokeType1;
+		pokeType2 = _pokeType2;
+
+		pokemonStat = GetStat();
+		curExp = 0;
+		nextExp = 999;
+		hp = maxHp;
+
+		isDead = false;
+	}
+	// 개체값 종족값 레벨을 계산해서 기본 스탯 반환
+	private PokemonStatS GetStat()
+	{
+		return new PokemonStatS(
+			hp: ((baseStat.hp * 2 + iv.hp) * level) / 100 + level + 10,
+			attack: ((baseStat.attack * 2 + iv.attack) * level) / 100 + 5,
+			defense: ((baseStat.defense * 2 + iv.defense) * level) / 100 + 5,
+			speAttack: ((baseStat.speAttack * 2 + iv.speAttack) * level) / 100 + 5,
+			speDefense: ((baseStat.speDefense * 2 + iv.speDefense) * level) / 100 + 5,
+			speed: ((baseStat.speed * 2 + iv.speed) * level) / 100 + 5
+		);
+	}
+}
