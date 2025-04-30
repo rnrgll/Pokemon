b965ff305e98c64b27d9988a5db40f1339f8b332
Clean18 <cleanage95@gmail.com>
2025-04-30 17:15:24 +0900

    [Feat] 이벤트 발생용 트리거 추가중
    플레이어집 1층
    연두마을
    이벤트 트리거 추가

--- Assets/SJH/EventScripts/TownExitEvent.cs
+++ Assets/SJH/EventScripts/TownExitEvent.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class TownExitEvent : PokeEvent
 {
-	public override void OnPokeEvent(Collider2D player)
+	public override void OnPokeEvent(GameObject player)
 	{
-		//@ 조건
-		// 포켓몬을 들고 있지 않은 상태에서 마을밖으로 나갈때
+		if (Manager.Poke.party.Count < 0)
+			return;
 
-		//@ 연관 엔피시
-		// NPC2
+		// TODO : 포켓몬 없이 마을밖으로 나갈 때 엔피시가 가로막게
 
-		//@ 흐름
-		// 플레이어 상태 바꾸기 UI
-		// UI 활성화
-		// 대사
-		// 엔피시가 플레이어 앞까지 이동
-		// 대사
-		// 플레이어와 함께 엔피시 원래 위치로 이동
+		// 장소 : 연두마을 -22, -12 or -22, -13
+		// 엔피시 : NPC2
+		// 조건 : 포켓몬을 들고 있지 않은 상태에서 마을밖으로 나갈때
+		if (player.transform.position.y == -12)
+		{
+
+		}
+		else
+		{
+
+		}
+		/*
+			(플레이어 y가 -12인기 -13인지 체크)
+
+			아! 골드군 (아무키입력대기)
+
+			if (y == -12)
+			(플레이어 → 방향 변경)
+			(엔피시 플레이어 앞으로 이동) -20, -12
+			(플레이어 엔피시 마주봄)
+
+			if (y == -13)
+			(플레이어 → 방향 변경
+			(엔피시 플레이어 위에 타일로 이동) -22, -12
+			(플레이어 엔피시 마주봄)
+
+			혼자서 어디 가니? (아무키입력대기)
+
+			(엔피시와 플레이어는 엔피시 원래 위치로 이동)
+
+			이런! 포켓몬도 지니지 않고
+			도로에 나가다니 위험해!
+
+			근처의 마을까지는
+			야생의 포켓몬이 튀어 나오는
+			풀숲만 있으니까
+		 */
 	}
 }
