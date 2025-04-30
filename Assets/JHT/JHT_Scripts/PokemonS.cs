cfc9ccc65f9639c87c16dc3d7024d945c1bf74c9
JangHyeonTae <141475959+JangHyeonTae@users.noreply.github.com>
2025-04-30 16:20:05 +0900

    [Feat] exp, hp Update
    포켓몬 hp, exp 업데이트를 구현

--- Assets/JHT/JHT_Scripts/PokemonS.cs
+++ Assets/JHT/JHT_Scripts/PokemonS.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,8 @@ public class PokemonS : MonoBehaviour
 	public int maxHp;
 	public int curExp;
 	public int nextExp;
+	private int maxLevel = 20;
+	
 	[SerializeField] public PokemonStatS baseStat;   // 종족값
 	[SerializeField] public PokemonIVS iv;           // 개체값
 													//public PokemonStat ev;			// 노력치
@@ -19,6 +22,9 @@ public class PokemonS : MonoBehaviour
 	public PokeType pokeType2;
 
 	public bool isDead;
+
+	public event Action OnDie;
+	public event Action OnLevelUp;
 
 	public PokemonS(int _id, string _name, int _level, PokemonStatS _baseStat, PokemonIVS _iv, PokeType _pokeType1, PokeType _pokeType2)
 	{
@@ -37,6 +43,18 @@ public class PokemonS : MonoBehaviour
 
 		isDead = false;
 	}
+
+	private void OnEnable()
+	{
+		OnDie += Die;
+		OnLevelUp += UpdateLevel;
+	}
+	private void OnDisable()
+	{
+		OnDie -= Die;
+		OnLevelUp -= UpdateLevel;
+	}
+
 	// 개체값 종족값 레벨을 계산해서 기본 스탯 반환
 	private PokemonStatS GetStat()
 	{
@@ -49,4 +67,86 @@ public class PokemonS : MonoBehaviour
 			speed: ((baseStat.speed * 2 + iv.speed) * level) / 100 + 5
 		);
 	}
+
+	public int GetNextExp(int level)
+	{
+		if (0 <= level && level < 5)
+			return level * level * 2;
+		else if (5 <= level && level < 10) 
+			return level * level * 4;
+		else if (10 <= level && level < 20)
+			return level * level * 8;
+		else
+			return level * level * 8;
+	}
+
+	public void GeNextExp(int amount)
+	{
+		curExp += amount;
+		while (curExp >= nextExp)
+		{
+			curExp -= nextExp;
+			OnLevelUp?.Invoke();
+		}
+
+	}
+
+	public void TakeDamage(PokemonS attacker, int amount)
+	{
+		if (isDead) return;
+
+
+		Debug.Log($"{amount}만큼 데미지를 받습니다");
+
+		hp = Mathf.Max(0, hp - amount);
+		Debug.Log($"현재 체력 : {hp}");
+
+		if (hp == 0)
+		{
+			OnDie?.Invoke();
+		}
+	}
+
+	public void Die()
+	{
+		isDead = true;
+		Debug.Log($"{gameObject.name}이 사망했습니다");
+		Destroy(this.gameObject);
+		if (PokemonManagerS.Get.party.Count > 0)
+		{
+			Debug.Log("다음 포켓몬을 꺼내겠습니다");
+		}
+		else
+		{
+			Debug.Log("포켓몬이 없습니다");
+			//battle씬에서 나감
+		}
+	}	
+
+	public void UpdateLevel()
+	{
+		if (level <= 20)
+		{
+			level++;
+
+			int oldMaxHp = maxHp;
+			pokemonStat = GetStat(); 
+
+			int newHp = maxHp - oldMaxHp;
+			Heal(newHp);
+		}
+		else
+		{
+			Debug.Log("최대레벨 입니다");
+			curExp = 0;
+		}
+	}
+
+	public int Heal(int amount)
+	{
+		hp += Mathf.Min(maxHp, hp + amount);
+		return hp;
+	}
+
+
 }
