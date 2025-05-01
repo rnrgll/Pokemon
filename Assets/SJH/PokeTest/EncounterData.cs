7481946ac583444f1f1a892877a543732266ca45
Clean18 <cleanage95@gmail.com>
2025-05-02 02:10:18 +0900

    [Feat] 랜덤인카운터 - 야생 포켓몬 출현 데이터 추가

--- Assets/SJH/PokeTest/EncounterData.cs
+++ Assets/SJH/PokeTest/EncounterData.cs
@@ -23,7 +23,69 @@ public class EncounterData
 					new WildEncounterData("꼬렛",   2, 3, 15),
 					new WildEncounterData("부우부", 2, 4, 85),
 				}
-			}
+			},
+			["Route46"] = new Dictionary<bool, List<WildEncounterData>>()
+			{
+				[true] = new List<WildEncounterData>() // 낮
+				{
+					new WildEncounterData("꼬렛",   2, 4, 25),
+					new WildEncounterData("깨비참", 2, 3, 35),
+					new WildEncounterData("꼬마돌", 2, 3, 40),
+				},
+				[false] = new List<WildEncounterData>() // 밤
+				{
+					new WildEncounterData("꼬렛",   2, 3, 55),
+					new WildEncounterData("꼬마돌", 2, 4, 45),
+				}
+			},
+			["Route30"] = new Dictionary<bool, List<WildEncounterData>>()
+			{
+				[true] = new List<WildEncounterData>() // 낮
+				{
+					new WildEncounterData("캐터피", 3, 4, 35),
+					new WildEncounterData("단데기", 4, 5, 15),
+					new WildEncounterData("뿔충이", 3, 4, 35),
+					new WildEncounterData("딱충이", 4, 5, 15),
+					new WildEncounterData("구구",   2, 4, 50),
+				},
+				[false] = new List<WildEncounterData>() // 밤
+				{
+					new WildEncounterData("꼬렛",   3, 4, 40),
+					new WildEncounterData("부우부", 4, 4, 30),
+					new WildEncounterData("페이검", 3, 3, 30),
+				}
+			},
+			["Route31"] = new Dictionary<bool, List<WildEncounterData>>()
+			{
+				[true] = new List<WildEncounterData>() // 낮
+				{
+					new WildEncounterData("캐터피", 4, 5, 35),
+					new WildEncounterData("단데기", 5, 6, 15),
+					new WildEncounterData("뿔충이", 4, 5, 35),
+					new WildEncounterData("딱충이", 5, 6, 15),
+					new WildEncounterData("구구",   3, 3, 30),
+					new WildEncounterData("모다피", 3, 3, 20),
+				},
+				[false] = new List<WildEncounterData>() // 밤
+				{
+					new WildEncounterData("꼬렛",   4, 5, 40),
+					new WildEncounterData("부우부", 5, 5, 10),
+					new WildEncounterData("페이검", 4, 4, 30),
+					new WildEncounterData("모다피", 3, 3, 20),
+				}
+			},
+			["SproutTower"] = new Dictionary<bool, List<WildEncounterData>>()
+			{
+				[true] = new List<WildEncounterData>() // 낮
+				{
+					new WildEncounterData("꼬렛",   3, 6, 100),
+				},
+				[false] = new List<WildEncounterData>() // 밤
+				{
+					new WildEncounterData("꼬렛",   3, 6, 15),
+					new WildEncounterData("고오스", 3, 6, 85),
+				}
+			},
 		};
 	}
 
