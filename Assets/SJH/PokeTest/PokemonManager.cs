76a2021b26886690718beaa62d273e3b1c9747cd
Clean18 <cleanage95@gmail.com>
2025-05-01 16:19:55 +0900

    [Fix] 포켓몬 매니저 - 파티에 포켓몬 추가 수정 / 상호작용으로 포켓몬 얻는 기능 수정

--- Assets/SJH/PokeTest/PokemonManager.cs
+++ Assets/SJH/PokeTest/PokemonManager.cs
@@ -13,31 +13,10 @@ public class PokemonManager : Singleton<PokemonManager>
 	public Dictionary<int, PokemonStat> GetBaseStat = new Dictionary<int, PokemonStat>();
 	public GameObject pokemonPrefab;
 
-	void Start()
-	{
-		// 포켓몬 추가
-		Pokémon pokemon1 = Instantiate(pokemonPrefab).GetComponent<Pokémon>();
-		pokemon1.Init(1, 5);
-		party.Add(pokemon1);
-
-		Pokémon pokemon2 = Instantiate(pokemonPrefab).GetComponent<Pokémon>();
-		pokemon2.Init("블레이범", 35);
-		party.Add(pokemon2);
-	}
-
-	public void AddStartingPokemon(string name)
-	{
-		// 스타팅 얻는 함수
-		Pokémon pokemon = Instantiate(pokemonPrefab).GetComponent<Pokémon>();
-		pokemon.Init(name, 5);
-
-		AddParty(pokemon);
-	}
-
-	public void AddPokemon(string name, int level)
+	public void AddPokemon(string pokeName, int level)
 	{
 		Pokémon pokemon = Instantiate(pokemonPrefab).GetComponent<Pokémon>();
-		pokemon.Init(name, level);
+		pokemon.Init(pokeName, level);
 
 		AddParty(pokemon);
 	}
@@ -52,12 +31,15 @@ public class PokemonManager : Singleton<PokemonManager>
 
 	private void AddParty(Pokémon pokemon)
 	{
+		pokemon.gameObject.name = pokemon.pokeName;
 		if (party.Count >= 6)
 		{
 			Debug.Log($"가지고 있는 포켓몬 초과 PC로");
 			pc.Add(pokemon);
 			return;
 		}
+
 		party.Add(pokemon);
+		Debug.Log($"골드은/는 {pokemon.pokeName} 을/를 얻었다!");
 	}
 }
