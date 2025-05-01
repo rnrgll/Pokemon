50d22ea460d6b1d8ab75175e97afe480c4e366f6
Clean18 <cleanage95@gmail.com>
2025-05-01 16:05:00 +0900

    [Feat] 포켓몬 매니저 - 파티에 포켓몬 추가하는 기능 추가

--- Assets/SJH/PokeTest/PokemonManager.cs
+++ Assets/SJH/PokeTest/PokemonManager.cs
@@ -9,6 +9,7 @@ public class PokemonManager : Singleton<PokemonManager>
 	
 	// 내파티
 	public List<Pokémon> party = new List<Pokémon>();
+	public List<Pokémon> pc = new List<Pokémon>();
 	public Dictionary<int, PokemonStat> GetBaseStat = new Dictionary<int, PokemonStat>();
 	public GameObject pokemonPrefab;
 
@@ -22,5 +23,41 @@ public class PokemonManager : Singleton<PokemonManager>
 		Pokémon pokemon2 = Instantiate(pokemonPrefab).GetComponent<Pokémon>();
 		pokemon2.Init("블레이범", 35);
 		party.Add(pokemon2);
+	}
+
+	public void AddStartingPokemon(string name)
+	{
+		// 스타팅 얻는 함수
+		Pokémon pokemon = Instantiate(pokemonPrefab).GetComponent<Pokémon>();
+		pokemon.Init(name, 5);
+
+		AddParty(pokemon);
+	}
+
+	public void AddPokemon(string name, int level)
+	{
+		Pokémon pokemon = Instantiate(pokemonPrefab).GetComponent<Pokémon>();
+		pokemon.Init(name, level);
+
+		AddParty(pokemon);
+	}
+
+	public void AddPokemon(int pokedex, int level)
+	{
+		Pokémon pokemon = Instantiate(pokemonPrefab).GetComponent<Pokémon>();
+		pokemon.Init(pokedex, level);
+
+		AddParty(pokemon);
+	}
+
+	private void AddParty(Pokémon pokemon)
+	{
+		if (party.Count >= 6)
+		{
+			Debug.Log($"가지고 있는 포켓몬 초과 PC로");
+			pc.Add(pokemon);
+			return;
+		}
+		party.Add(pokemon);
 	}
 }
