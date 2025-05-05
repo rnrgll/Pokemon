2c2ae2edab47d0112c4a6804a5027f93ab55cc37
rnrgll <shuajjjjj@naver.com>
2025-05-06 00:14:05 +0900

    [Chore] 테스트를 위해 변경한 포켓몬 데이터 복구

--- Assets/SJH/PokeTest/SJH_PokemonData.cs
+++ Assets/SJH/PokeTest/SJH_PokemonData.cs
@@ -314,9 +314,7 @@ public class SJH_PokemonData
 				EvolveName = "장크로다일",
 				DefaultSkill = new List<string>()
 				{
-					//"할퀴기", "째려보기", "분노"
-					//임시테스트데이터
-					"플래시", "째려보기", "분노", "햘퀴기"
+					"할퀴기", "째려보기", "분노"
 				},
 				SkillDic = new Dictionary<int, string>()
 				{
