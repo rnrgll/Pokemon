de9b5164a4a9e90383a1becb8f357f4dfe0f3089
KyuSSeo <kyuri2665@gmail.com>
2025-05-07 06:00:55 +0900

    [Feat] 나무열매 아저씨 이벤트 완료

--- Assets/SJH/EventScripts/BerryHouseEvent.cs
+++ Assets/SJH/EventScripts/BerryHouseEvent.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class BerryHouseEvent : PokeEvent
 {
+	[Header("대화 관련 설정")]
+	[SerializeField] private Dialog dialog;
+	[SerializeField] private GameObject npc;
 	public override void OnPokeEvent(GameObject player)
 	{
-		/*
-		 포켓몬이 나무열매를 먹는다는거 
-		너 알고 있니?
-		우리집의 포켓몬은 
-		나무열매를 먹으면 건강해진단다
-		그래! 너에게도 나누어줄게
+		Debug.Log($"{Manager.Event.berryHouseEvent} 상태");
+		if (Manager.Event.berryHouseEvent)
+		{
+			dialog = new Dialog(new List<string>
+			{
+				"나무열매를 조사하면",
+				"나무열매가 떨어질꺼야"
+			});
+			Manager.Dialog.StartDialogue(dialog);
+			return;
+		}
+		StartCoroutine(TriggerDialogue());
+		Manager.Event.berryHouseEvent = true;
+	}
+	private IEnumerator TriggerDialogue()
+	{
+		Manager.Dialog.StartDialogue(dialog);
 
-		골드는 
-		나무열매를 얻었다
-		 
-		골드는 나무열매를
-		도구 포켓에 넣었다.
-
-
-
-		이후 대화
-		나무열매를 조사하면
-		나무열매가 떨어질꺼야
-
-		 */
-
+		while (Manager.Dialog.isTyping)
+		{
+			yield return null;
+		}
 	}
 }
