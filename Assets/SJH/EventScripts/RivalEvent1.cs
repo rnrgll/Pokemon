21bc1697903d27678e78e1d16f4f28c184a532b5
KyuSSeo <kyuri2665@gmail.com>
2025-05-07 01:48:56 +0900

    [Test] 이벤트 대화 움직임 중간정리

---
+++ Assets/SJH/EventScripts/RivalEvent1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RivalEvent1 : PokeEvent
+{
+	public override void OnPokeEvent(GameObject player)
+	{
+		/*
+			......
+		이곳이 유명한 
+		공박사 포켓몬 연구소...
+		뭐야
+		사람을 뚫어지게 쳐다보고
+
+		플레이어 라이벌 동시에 x축 -1칸
+		플레이어만 y축 -3칸
+		라이벌 x축 +1칸
+		
+		 */
+	}
+}
