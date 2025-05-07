6b847b5ee8d6207559e2c74ab1fc13834ee0a481
KyuSSeo <kyuri2665@gmail.com>
2025-05-07 12:39:01 +0900

    [Feat] 이벤트 중간 커밋

--- Assets/SJH/EventScripts/StarterEvent.cs
+++ Assets/SJH/EventScripts/StarterEvent.cs
@@ -5,8 +5,45 @@ using UnityEngine;
 public class StarterEvent : PokeEvent
 {
 
+	[SerializeField] private Dialog dialog;
+	[SerializeField] Vector2 targetPos;  // 목표 위치
+
+
 	public override void OnPokeEvent(GameObject player)
 	{
+		Debug.Log("스타팅 선택 시작!");
+		if (Manager.Event.starterEvent) {
+			Debug.Log("스타팅 선택 이미 진행됨!");
+			dialog = new Dialog(new List<string>
+			{
+				"포켓몬이 들어 있는 몬스터 볼이다!"
+			});
+
+			StartCoroutine(TriggerDialogue());
+			return;
+		}
+
+
+		Debug.Log("대화창!");
+		dialog = new Dialog(new List<string>
+		{
+				"포켓몬 할아버지가 계신 곳은",
+				"이마을 근처의",
+				"무궁시티 끝이란다",
+				"거기까지는 거의 외길이니까",
+				"찾기 쉬울꺼다",
+				"포켓몬이 상처를 입었다면",
+				"저쪽에 있는 기계로",
+				"포켓몬을 회복시켜주거라",
+				"참 내 전화번호를",
+				"알려줄테니까",
+				"무슨 일이 있으면 연락하거라!",
+				"골드는 공박사님의",
+				"전화번호를 등록했다"
+		});
+		StartCoroutine(TriggerDialogue());
+		Debug.Log("스타팅 종료!!");
+		Manager.Event.starterEvent = true;
 		/* 
 		포켓몬 선택지 대사
 		
@@ -22,24 +59,15 @@ public class StarterEvent : PokeEvent
 		콜드는 공박사한테
 		player.name은 공박사한테
 		okemon.name을 받았다
-
-		받은 pokemon.name에게
-		이름을 붙이겠습니까? 예/아니오
-			
-		공박사에게 다가감
-		포켓몬 할아버지가 계신 곳은 
-		이마을 근처의
-		무궁시티 끝이란다
-		거기까지는 거의 외길이니까
-		찾기 쉬울꺼다
-		포켓몬이 상처를 입었다면
-		저쪽에 있는 기계로
-		포켓몬을 회복시켜주거라
-		참 내 전화번호를 
-		알려줄테니까
-		무슨 일이 있으면 연락하거라!
-		player.name은 공박사님의 
-		전화번호를 등록했다
 		 */
+	}
+	private IEnumerator TriggerDialogue()
+	{
+		Manager.Dialog.StartDialogue(dialog);
+
+		while (Manager.Dialog.isTyping)
+		{
+			yield return null;
+		}
 	}
 }
