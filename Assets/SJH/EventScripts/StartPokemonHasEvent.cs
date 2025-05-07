f3c80c7e75c5c35b537f183768ff80ab58ebf1a6
KyuSSeo <kyuri2665@gmail.com>
2025-05-07 11:44:16 +0900

    [Test] 공박사 연구소 이벤트

---
+++ Assets/SJH/EventScripts/StartPokemonHasEvent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPokemonHas : MonoBehaviour
+{
+	/*
+	 포켓몬 미분양 나가기 시도 시 
+			(-12,8), (-10, 8)
+			잠깐 잠깐 어디에 가느냐!
+			플레이어 한 칸 위로
+	 */
+}
