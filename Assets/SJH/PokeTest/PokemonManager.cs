0e66171e684904b22c7e42fe62b7244c2ec327cc
Clean18 <cleanage95@gmail.com>
2025-05-08 10:37:13 +0900

    [Fix] 맵46 > 29게이트 맵이동 에러 수정

--- Assets/SJH/PokeTest/PokemonManager.cs
+++ Assets/SJH/PokeTest/PokemonManager.cs
@@ -28,7 +28,7 @@ public class PokemonManager : Singleton<PokemonManager>
 	void Start()
 	{
 		// TODO :  삭제
-		AddPokemon("리아코", 30);
+		//AddPokemon("리아코", 30);
 	}
 
 	public void AddPokemon(string pokeName, int level)
