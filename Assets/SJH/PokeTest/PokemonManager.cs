3d6059b466f3437ced8ffba94788183025fe4120
Clean18 <cleanage95@gmail.com>
2025-05-08 09:59:24 +0900

    [Feat] 관장 클리어 트리거 추가

--- Assets/SJH/PokeTest/PokemonManager.cs
+++ Assets/SJH/PokeTest/PokemonManager.cs
@@ -28,7 +28,7 @@ public class PokemonManager : Singleton<PokemonManager>
 	void Start()
 	{
 		// TODO :  삭제
-		//AddPokemon("블레이범", 99);
+		AddPokemon("블레이범", 99);
 	}
 
 	public void AddPokemon(string pokeName, int level)
